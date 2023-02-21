1428552f7e1dd70b4f9ca99c5d33a30d914d047f
SiwarZagouji <zagoujisiwar@gmail.com>
2023-02-21 05:29:32 -0800

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
 /**
- *a program that prints _putchar, followed by a new line.
- */
-
-/**
  * main - entry point
- * return: always 0: success
+ *
+ * return: always 0 success
  */
 int main(void)
 {       _putchar('_');
