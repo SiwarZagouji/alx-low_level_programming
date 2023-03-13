c0de30641c5baa00f10a040a91ae7cdcd2fa95d1
SiwarZagouji <zagoujisiwar@gmail.com>
2023-03-13 09:18:15 -0700

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
-  * print_rev - appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte
+  * _strcat - appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte
+  * @dest: input value
+  * @src: input value
   * @p: pointer to the beginning of the destination
   *
   * Return: char *
